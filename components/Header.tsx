8428ff08a05e85f9e744e5ea5ea121f14aa9a7ab
Matheus Cerqueira <matheus@shockit.com.br>
2026-03-17 14:34:59 -0300

    att header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,42 @@
-'use client'
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
+"use client";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <Navbar fixed="top" expand="lg" className="glass-card" style={{ margin: '10px', borderRadius: '16px' }}>
+    <Navbar
+      fixed="top"
+      expand="lg"
+      className="glass-card"
+      style={{ margin: "10px", borderRadius: "16px" }}
+    >
       <Container>
-        <Navbar.Brand href="#home" style={{ fontFamily: 'Poppins', fontWeight: 700, color: '#880e4f' }}>
+        <Navbar.Brand
+          href="#home"
+          style={{ fontFamily: "Poppins", fontWeight: 700, color: "#880e4f" }}
+        >
           Ana Caroline
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#sobre" style={{ color: '#880e4f' }}>Sobre</Nav.Link>
-            <Nav.Link href="#servicos" style={{ color: '#880e4f' }}>Serviços</Nav.Link>
-            <Nav.Link href="#galeria" style={{ color: '#880e4f' }}>Galeria</Nav.Link>
-            <Nav.Link href="#depoimentos" style={{ color: '#880e4f' }}>Depoimentos</Nav.Link>
-            <Nav.Link href="#contato" style={{ color: '#880e4f' }}>Contato</Nav.Link>
+            <Nav.Link href="#sobre" style={{ color: "#880e4f" }}>
+              Sobre
+            </Nav.Link>
+            <Nav.Link href="#servicos" style={{ color: "#880e4f" }}>
+              Serviços
+            </Nav.Link>
+            <Nav.Link href="#galeria" style={{ color: "#880e4f" }}>
+              Galeria
+            </Nav.Link>
+            {/* <Nav.Link href="#depoimentos" style={{ color: '#880e4f' }}>Depoimentos</Nav.Link> */}
+            <Nav.Link href="#contato" style={{ color: "#880e4f" }}>
+              Contato
+            </Nav.Link>
             <Nav.Link
               href="https://www.instagram.com/anacarol_sol/"
               target="_blank"
-              style={{ marginLeft: '10px', color: '#ec407a' }}
+              style={{ marginLeft: "10px", color: "#ec407a" }}
               title="Instagram"
             >
               <FaInstagram size={22} />
@@ -29,5 +45,5 @@ export default function Header() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
